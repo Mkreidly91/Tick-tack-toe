5fb7a8f9a081bbd1848ddd3ff1b9c595039cdfdd
Mostafa Kreidly <mostafa@Mostafas-MacBook-Air.local>
2023-07-12 11:01:02 +0300

    added checkWinner function, to be optimised

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ for (let i = 0; i < boxes.length; i++) {
 }
 
 const winningCombinations = [
+  // '012',
+  // '345',
+  // '678',
+  // '036',
+  // '147',
+  // '258',
+  // '048',
+  // '246',
   // Horizontal Rows
   [0, 1, 2],
   [3, 4, 5],
@@ -21,21 +29,59 @@ const winningCombinations = [
   [2, 4, 6],
 ];
 
+let turn = 'x';
+let player1_moves = [];
+let player2_moves = [];
+
 let visited = [];
-let Tick = [];
-let Tack = [];
-function addTick(e) {
-  const target = e.value.target;
-  visited.push(target.id);
 
-  addTack(target.id);
+function updateBlock(e) {
+  const target = e.target;
+  const cellId = Number(target.id);
+  if (visited.includes(cellId)) return;
+  visited.push(cellId);
+  if (turn === 'x') {
+    player1_moves.push(cellId);
+    target.innerText = 'x';
+  } else {
+    player2_moves.push(cellId);
+    target.innerText = 'o';
+  }
+  checkWinner();
+  turn = turn === 'x' ? 'o' : 'x';
 }
+function checkWinner() {
+  console.log(player1_moves);
+  console.log(player2_moves);
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const p1Wins = winningCombinations[i].every((element) =>
+      player1_moves.includes(element)
+    );
+    const p2Wins = winningCombinations[i].every((element) =>
+      player2_moves.includes(element)
+    );
+    console.log(p1Wins, p2Wins);
+    if (p1Wins) {
+      console.log('Player 1 wins');
+    }
+    if (p2Wins) {
+      console.log('Player 2 wins');
+    }
+  }
 
-// vs AI
-function addTack(id) {
-  const unvisited = visited.filter((element) => element !== id);
-  //choose a random element in unvisited
-  const randomIndex = Math.floor(Math.random() * array.length);
-  visited.push();
-  boxes[randomIndex].innerHTML = tick;
+  //   if (player1_moves.includes(winningCombinations[i])) {
+  //   } else if (player2_moves.includes(winningCombinations[i])) {
+  //   }
+  // }
 }
+function gameStatus() {}
+
+document.addEventListener('click', updateBlock);
+// // vs AI
+// function addTack(id) {
+//   const unvisited = visited.filter((element) => element !== id);
+//   //choose a random element in unvisited
+//   const randomIndex = Math.floor(Math.random() * array.length);
+//   visited.push();
+//   boxes[randomIndex].innerHTML = tick;
+// }
